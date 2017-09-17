f3233543a6142bff7e07e640e8462a449b0f2b83
aidarbek <aidarbek98@gmail.com>
2017-09-17 11:06:09 +0600

    Accuracy increased

--- train.py
+++ train.py
@@ -8,26 +8,30 @@ import codecs
 import numpy as np
 from scipy.sparse import coo_matrix, vstack
 def normalize(s):
-	res = re.sub("[^\w]", " ",  s.lower())
-	res = ' '.join(res.split())
-	return res
+	sentences = s.split(".")
+	ans = []
+	for s in sentences:
+		res = re.sub("[^\w]", " ",  s.lower())
+		ans.append(' '.join(res.split()))
+	return ans
 
 f = open("train.txt", "rb")
 answers = f.read()
 f.close()
 
 answers = answers.replace("\r", "\n").split("\n\n")
-answers = [ans for ans in answers if len(normalize(ans).split()) >= 6]
-
-norm_answers = [normalize(ans).replace("kazakhstan", "") for ans in answers]
-norm_answers = [ans for ans in norm_answers if len(ans.split()) >= 5]
-
-norm_answers = norm_answers * 1
+answers = [ans for ans in answers if len(re.sub("[^\w]", " ",  ans.lower()).split()) >= 5]
+norm_answers = [normalize(ans) for ans in answers]
+Y = []
+X = []
+for i in range(len(norm_answers)):
+	for sentence in norm_answers[i]:
+		Y.append(i)
+		X.append(sentence)
+X = X * 1
 
 vectorizer = CountVectorizer()
-train_answers = vectorizer.fit_transform(norm_answers)
-Y = [i for i in range(len(answers))]
-Y = Y * 1
+train_answers = vectorizer.fit_transform(X)
 
 Y = np.array(Y)
 
@@ -42,14 +46,14 @@ def generate_answer(userInput):
 	no_ans = "Sorry, I didn't understand your request"
 	try:
 		userInput = unicode(userInput, "utf-8")
-		words = normalize(userInput)
+		words = " ".join(normalize(userInput))
 		user_vector = vectorizer.transform([words])
 		ans = answers[regr.predict(user_vector)[0]]
 		prob = regr.predict_proba(user_vector[0])
 		if np.max(prob) < 0.02:
 			ans = no_ans
 		return ans
-	except:
+	except Exception as e:
 		return no_ans
 def main():
 	print(generate_answer("What is JSC?"))
@@ -72,11 +76,11 @@ def main():
 	print("------")
 	print(generate_answer("What does include the special economic zone (SEZ) Khorgos-Eastern Gate"))
 	print("------")
-	print(generate_answer("When the Republic of Kazakhstan gained the independence")) # x
+	print(generate_answer("When the Republic of Kazakhstan gained the independence"))
 	print("------")
-	print(generate_answer("What are official business hours in Kazakhstan?"))  # x
+	print(generate_answer("When Kazakhstan offices are open?"))  # x
 	print("------")
-	print(generate_answer("Time zones of Kazakhstan in relevance to Greenwinch zone"))  # x
+	print(generate_answer("Time zones of Kazakhstan in relevance to Greenwinch zone"))
 	print("------")
 	print(generate_answer("What is the principle of Kazakhstan state registration?")) # x
 	print("------")
@@ -86,13 +90,16 @@ def main():
 	print("------")
 	print(generate_answer("What is included in taxable income?"))
 	print("------")
-	print(generate_answer("Length of maternity leave")) # X
+	print(generate_answer("Length of maternity leave"))
 	print("------")
 	print(generate_answer("additional paid vacation dated for people working in hazardous conditions"))
 	print("------")
 	print(generate_answer("how work permits are granted?"))
 	print("------")
-	print(generate_answer(u"КОгда у Айдарбека месячные?")) # X
+	print(generate_answer("What does VAT mean?"))
+	print("------")
+	print(generate_answer(u"КОгда у Айдарбека месячные?"))
+	print("------")
 
 if __name__ == "__main__":
 	main()
